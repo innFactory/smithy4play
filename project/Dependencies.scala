0d90173ea0ba2ff0a1d9076e595c7326b9860e11
Tobias Jonas <t.jonas@innfactory.de>
2022-10-28 11:47:18 +0000

    Update smithy4s-core, smithy4s-json to 0.16.6

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -7,7 +7,7 @@ object Dependencies {
   val typesafePlay = "com.typesafe.play" %% "play" % playVersion
 
   val scalaVersion    = "2.13.8"
-  val smithy4sVersion = "0.16.5"
+  val smithy4sVersion = "0.16.6"
   val smithyCore      = "com.disneystreaming.smithy4s" %% "smithy4s-core" % smithy4sVersion
   val smithyJson      = "com.disneystreaming.smithy4s" %% "smithy4s-json" % smithy4sVersion
 
