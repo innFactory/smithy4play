1a62c2838d3402321f800324e7a84ae89dee5f37
Tobias Jonas <t.jonas@innfactory.de>
2022-11-18 00:22:03 +0000

    chore: update classgraph from 4.8.150 to 4.8.151

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -11,7 +11,7 @@ object Dependencies {
   val smithyCore      = "com.disneystreaming.smithy4s" %% "smithy4s-core" % smithy4sVersion
   val smithyJson      = "com.disneystreaming.smithy4s" %% "smithy4s-json" % smithy4sVersion
 
-  val classgraph    = "io.github.classgraph" % "classgraph" % "4.8.150"
+  val classgraph    = "io.github.classgraph" % "classgraph" % "4.8.151"
   val smithyVersion = "1.26.2"
 
   val testTraits =
