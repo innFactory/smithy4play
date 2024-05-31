d00a670ff5a3cd9ba5301bb4b34cc44eca85bc51
Tobias Jonas <t.jonas@innfactory.de>
2024-05-31 00:19:22 +0000

    chore: update alloy-core, alloy-openapi from 0.3.6 to 0.3.9

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -12,8 +12,8 @@ object Dependencies {
   val smithyJson         = "com.disneystreaming.smithy4s" %% "smithy4s-json"             % smithy4sVersion
   val smithyXml          = "com.disneystreaming.smithy4s" %% "smithy4s-xml"              % smithy4sVersion
   val smithy4sCompliance = "com.disneystreaming.smithy4s" %% "smithy4s-compliance-tests" % smithy4sVersion
-  val alloyCore          = "com.disneystreaming.alloy"     % "alloy-core"                % "0.3.6"
-  val alloyOpenapi       = "com.disneystreaming.alloy"    %% "alloy-openapi"             % "0.3.6"
+  val alloyCore          = "com.disneystreaming.alloy"     % "alloy-core"                % "0.3.9"
+  val alloyOpenapi       = "com.disneystreaming.alloy"    %% "alloy-openapi"             % "0.3.9"
 
   val classgraph    = "io.github.classgraph" % "classgraph" % "4.8.172"
   val smithyVersion = "1.48.0"
