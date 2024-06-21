a18e582b2aa75e51aac2947ebf37e580eb2a9f81
Tobias Jonas <t.jonas@innfactory.de>
2024-06-21 00:18:42 +0000

    chore: update smithy-protocol-test-traits from 1.49.0 to 1.50.0

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -16,7 +16,7 @@ object Dependencies {
   val alloyOpenapi       = "com.disneystreaming.alloy"    %% "alloy-openapi"             % "0.3.9"
 
   val classgraph    = "io.github.classgraph" % "classgraph" % "4.8.174"
-  val smithyVersion = "1.49.0"
+  val smithyVersion = "1.50.0"
   val testTraits    =
     "software.amazon.smithy" % "smithy-protocol-test-traits" % smithyVersion
 
