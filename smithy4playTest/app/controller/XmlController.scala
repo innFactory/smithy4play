4358103098691993fa75e9cd2d15fd8d24e60a64
Lintterer <lintt@gmx.de>
2024-06-24 14:25:11 +0200

    feat: ignoring charset in content-type for encoding and decoding

--- smithy4playTest/app/controller/XmlController.scala
+++ smithy4playTest/app/controller/XmlController.scala
@@ -4,7 +4,13 @@ import cats.data.{ EitherT, Kleisli }
 import cats.implicits.catsSyntaxEitherId
 import de.innfactory.smithy4play.{ AutoRouting, ContextRoute, ContextRouteError }
 import play.api.mvc.ControllerComponents
-import testDefinitions.test.{ XmlControllerDef, XmlTestInputBody, XmlTestOutput, XmlTestWithInputAndOutputOutput }
+import testDefinitions.test.{
+  XmlControllerDef,
+  XmlTestInputBody,
+  XmlTestOutput,
+  XmlTestWithCharsetOutput,
+  XmlTestWithInputAndOutputOutput
+}
 
 import javax.inject.{ Inject, Singleton }
 import scala.concurrent.{ ExecutionContext, Future }
@@ -18,16 +24,19 @@ class XmlController @Inject() (implicit
 
   override def xmlTestWithInputAndOutput(
     xmlTest: String,
-    body: XmlTestInputBody
+    body: XmlTestInputBody,
+    contentType: Option[String]
   ): ContextRoute[XmlTestWithInputAndOutputOutput] =
     Kleisli { _ =>
       EitherT(
         Future(
           XmlTestWithInputAndOutputOutput(
-            XmlTestOutput(body.serverzeit, body.requiredTest + xmlTest, body.requiredInt.map(i => i * i))
+            XmlTestOutput(body.serverzeit, body.requiredTest + xmlTest, body.requiredInt.map(i => i * i)),
+            contentType
           )
             .asRight[ContextRouteError]
         )
       )
     }
+
 }
