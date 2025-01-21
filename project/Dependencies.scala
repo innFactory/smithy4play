48ad28ec79a37be6492a411c1be8f5ef788b6561
Tobias Jonas <t.jonas@innfactory.de>
2025-01-21 00:21:55 +0000

    chore: update cats-core from 2.12.0 to 2.13.0

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -22,7 +22,7 @@ object Dependencies {
 
   val scalatestPlus =
     "org.scalatestplus.play" %% "scalatestplus-play" % "5.1.0" % Test
-  val cats = "org.typelevel" %% "cats-core" % "2.12.0"
+  val cats = "org.typelevel" %% "cats-core" % "2.13.0"
 
   lazy val list = Seq(
     smithyCore,
